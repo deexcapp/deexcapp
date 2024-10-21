abe90f719a46bbb36b48019b8142f21753556e48
deexcapp <deexcapp@gmail.com>
2024-10-22 01:41:05 +0600

    Update bot.php

--- salektab/bot.php
+++ salektab/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "8071907152:AAFJlm7d7kW54QLQRW5UfUF3upzNgkY7R48";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
